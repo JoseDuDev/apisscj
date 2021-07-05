ed71c5df625f93b03f1ae21fad786cd4a2cd932e
Jose Eduardo LS <josedudev@gmail.com>
2021-07-05 14:12:10 -0300

    correção de rotas de base

--- routes/base.js
+++ routes/base.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const cargo = require('../services/cargo');
+const base = require('../services/base');
 
 /* GET programming languages. */
 router.get('/', async function(req, res, next) {
     try {
-        res.json(await cargo.get());
+        res.json(await base.get());
     } catch (err) {
         console.error(`Erro ao obter base`, err.message);
         next(err);
@@ -15,7 +15,7 @@ router.get('/', async function(req, res, next) {
 /* PUT programming language */
 router.put('/:id', async function(req, res, next) {
     try {
-        res.json(await cargo.update(req.params.id, req.body));
+        res.json(await base.update(req.params.id, req.body));
     } catch (err) {
         console.error(`Erro ao atualizar base`, err.message);
         next(err);
